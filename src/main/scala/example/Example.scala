75be7e700780959b0b4babdd86bc590bf938b0b7
Claudiu Creanga <danutempyrium@yahoo.com>
2017-03-06 00:20:39 +0000

    nothing here

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -5,5 +5,13 @@ package example
   */
 object Example extends App{
   println("Hello World")
+  def sqrtIter(guess: Double, x: Double): Double =
+    if (isGoodEnough(guess, x)) guess
+    else sqrtIter(improve(guess, x), x)
 
+  def isGoodEnough (guess: Double, x: Double): Boolean =
+    abs(guess * guess - x) < 0.001
+
+  def improve(guess: Double, x: Double): Double =
+    (guess + x / guess) / 2
 }
