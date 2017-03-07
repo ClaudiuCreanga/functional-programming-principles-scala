c89bfb53716593d00ae3a078e57c9f1f6e7440e9
Claudiu Creanga <claudicreanga@Salamis.local>
2017-03-07 18:48:06 +0000

    nothing here

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -1,17 +1,41 @@
 package example
 
+import math._
 /**
   * Created by Claudiu on 09/02/2017.
   */
 object Example extends App{
-  println("Hello World")
-  def sqrtIter(guess: Double, x: Double): Double =
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
 
-  def isGoodEnough (guess: Double, x: Double): Boolean =
-    abs(guess * guess - x) < 0.001
+  def sqrt(x: Double) = {
+    def sqrtIter(guess: Double): Double =
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
 
-  def improve(guess: Double, x: Double): Double =
-    (guess + x / guess) / 2
+    def isGoodEnough(guess: Double): Boolean =
+      abs(guess * guess - x) < 0.001
+
+    def improve(guess: Double): Double =
+      (guess + x / guess) / 2
+
+    sqrtIter(1.0)
+  }
+  println(sqrt(4))
+
+  def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+  println(gcd(14, 21))
+
+  def tailRecursiveFactorial(n: Int): Int = {
+    def loop(acc: Int, n: Int): Int =
+      if (n==0) acc
+      else loop(acc * n, n-1)
+    loop(1, n)
+  }
+
+  println(tailRecursiveFactorial(4))
+
+  def pascalBuild(x: Int) =
+    
+  println("3 \n 5"+4)
 }
